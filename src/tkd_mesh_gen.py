e70c8a12a0fe26c7db2941a895150536a97d7aa8
pzuritas <pzurita@uc.cl>
2021-08-09 13:16:49 -0400

    :construction: Hollow TKD generation with Netgen

--- src/tkd_mesh_gen.py
+++ src/tkd_mesh_gen.py
@@ -2,6 +2,9 @@ from ngsolve import *
 from netgen.csg import *
 
 
+L = 1
+h = 0.1
+
 n_1 = Vec(1, 1, 1)
 n_2 = Vec(1, -1, 1)
 n_3 = Vec(-1, 1, 1)
@@ -12,10 +15,10 @@ n_6 = Vec(1, -1, -1)
 n_7 = Vec(-1, 1, -1)
 n_8 = Vec(-1, -1, -1)
 
-p_1 = Pnt(1, 1, 0)
-p_2 = Pnt(1, -1, 0)
-p_3 = Pnt(-1, 1, 0)
-p_4 = Pnt(-1, -1, 0)
+p_1 = Pnt(L, L, 0)
+p_2 = Pnt(L, -L, 0)
+p_3 = Pnt(-L, L, 0)
+p_4 = Pnt(-L, -L, 0)
 
 up_left = Plane(p_1, n_1)
 up_front = Plane(p_2, n_2)
@@ -31,12 +34,39 @@ octahedron = up_left*up_right*up_front*up_back\
     *bot_left*bot_right*bot_front*bot_back
 
 cube = OrthoBrick(
-    Pnt(-1, -1, -1),
-    Pnt(1, 1, 1)
+    Pnt(-L, -L, -L),
+    Pnt(L, L, L)
 )
+
+p_5 = Pnt(L-h, L-h, 0)
+p_6 = Pnt(L-h, -L+h, 0)
+p_7 = Pnt(-L+h, L-h, 0)
+p_8 = Pnt(-L+h, -L+h, 0)
+
+mini_up_left = Plane(p_5, n_1)
+mini_up_front = Plane(p_6, n_2)
+mini_up_right = Plane(p_7, n_3)
+mini_up_back = Plane(p_8, n_4)
+
+mini_bot_left = Plane(p_5, n_5)
+mini_bot_front = Plane(p_6, n_6)
+mini_bot_right = Plane(p_7, n_7)
+mini_bot_back = Plane(p_8, n_8)
+
+mini_octahedron = mini_up_left*mini_up_right*mini_up_front*mini_up_back\
+    *mini_bot_left*mini_bot_right*mini_bot_front*mini_bot_back
+
+mini_cube = OrthoBrick(
+    Pnt(-L+h, -L+h, -L+h),
+    Pnt(L-h, L-h, L-h)
+)
+
+hole = mini_octahedron*mini_cube
 
 tkd = octahedron*cube
 
+alv = tkd - hole
+
 geo = CSGeometry()
-geo.Add(tkd)
+geo.Add(alv)
 mesh = geo.GenerateMesh(maxh=0.1)
