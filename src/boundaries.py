cb3d729e56af194c6a442bb82db30bd0e8023b46
pzuritas <pzurita@uc.cl>
2021-08-22 23:55:44 -0400

    :construction: Changed periodic boundaries to allow for TKD

--- src/boundaries.py
+++ src/boundaries.py
@@ -73,13 +73,13 @@ class GammaAir(SubDomain):
                 or near(x[0], self.dir_max, self.tol)
         )
 
-class GammaPi(SubDomain):
+class GammaSlabPi(SubDomain):
     '''Subdomain class for periodic boundary conditions in slab mesh.'''
     def __init__(self, dir_min, dir_max, tol):
         '''Instance the subdomain.
         
-        dir_min: minimum value of periodic direction. (float)
-        dir_max: maximum value of periodic direction. (float)
+        dir_min: minimum value of periodic direction (z). (float)
+        dir_max: maximum value of periodic direction (z). (float)
         tol: tolerance for numerical roundoff in element tagging. (float)
         '''
         super().__init__()
@@ -105,9 +105,9 @@ class GammaPi(SubDomain):
         y[1] = x[1]
         y[2] = x[2] - (self.dir_max - self.dir_min)
 
-class GammaAir_Pi(GammaAir):
+class GammaAirSlabPi(GammaAir):
     '''Alternative subdomain class for \Gamma_{\text{air}} when using
-    periodic boundary conditions.
+    periodic boundary conditions on slab mesh.
     '''
     def __init__(self, dir_min, dir_max, tol):
         '''Instance the subdomain.
@@ -128,3 +128,56 @@ class GammaAir_Pi(GammaAir):
             near(x[1], self.dir_max, self.tol) \
                 or near(x[1], self.dir_min, self.tol)
         )
+
+class GammaTKDPi(SubDomain):
+    '''Subdomain class for periodic boundary conditions in TKD mesh.'''
+    def __init__(self, dir_min, dir_max, tol):
+        '''Instance the subdomain.
+        
+        dir_min: minimum value of periodic direction (z or y). (float)
+        dir_max: maximum value of periodic direction (z or y). (float)
+        tol: tolerance for numerical roundoff in element tagging. (float)
+        '''
+        super().__init__()
+        self.dir_min = dir_min
+        self.dir_max = dir_max
+        self.tol = tol
+
+    def inside(self, x, on_boundary):
+        '''Checks if position is on subdomain.
+        
+        x: position.
+        on_boundary: True if on element boundary. (bool)
+        '''
+        return on_boundary and (
+            near(x[1], self.dir_max, self.tol) \
+                or near(x[2], self.dir_max, self.tol)
+        )
+
+    def map(self, x, y):
+        '''Maps opposite faces for periodic boundary conditions.
+        
+        x: position in subdomain.
+        y: position in opposite subdomain.
+        '''
+        y[0] = x[0]
+        if near(x[1], self.dir_max, self.tol):
+            y[1] = x[1] - (self.dir_max - self.dir_min)
+            y[2] = x[2]
+        else:
+            y[1] = x[1]
+            y[2] = x[2] - (self.dir_max - self.dir_min)
+
+class GammaAirTKD(SubDomain):
+    '''Subdomain class for periodic boundary conditions in slab mesh.'''
+    def __init__(self, dir_min, dir_max, tol):
+        '''Instance the subdomain.
+        
+        dir_min: minimum value of periodic direction. (float)
+        dir_max: maximum value of periodic direction. (float)
+        tol: tolerance for numerical roundoff in element tagging. (float)
+        '''
+        super().__init__()
+        self.dir_min = dir_min
+        self.dir_max = dir_max
+        self.tol = tol
