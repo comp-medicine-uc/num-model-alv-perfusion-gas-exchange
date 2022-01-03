1923d0ca25976bd24e4b931f6b6da818b5289c89
pzuritas <pzurita@uc.cl>
2022-01-03 14:45:27 -0300

    :rocket: Main file for cluster simulations

---
+++ tests/balloon_main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import dolfin
+sys.path.append(os.getcwd()[:-6])
+from src.model import PerfusionGasExchangeModel
+from src.params import params
+
+mesh_path = "sphere_fine.xml"
+folder = "sphere_fine_sims"
+path = os.path.join("../raw-data", folder)
+model = PerfusionGasExchangeModel(folder_path=path, params=params)
+model.import_mesh(mesh_path, type="xml")
+model.sim_p(save=True, meshtype="sphere")
+boundaries = dolfin.File(model.folder_path+'/bnd/bnd.pvd')
+boundaries << model.boundaries
+x = model.sim_sbst(hb=False, save=False)
+solution = model.sim_sbst(hb=True, save=True, guess=x)
