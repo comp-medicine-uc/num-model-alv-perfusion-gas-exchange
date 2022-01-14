8f4fdb66f51c3bedc8f794f6ea89f963d3c71fc6
pzuritas <pzurita@uc.cl>
2022-01-14 20:57:08 -0300

    :adhesive_bandage: Added prints

--- tests/balloon_main.py
+++ tests/balloon_main.py
@@ -13,15 +13,22 @@ from src.model import PerfusionGasExchangeModel
 from src.params import params
 
 
+print("Starting...")
 folder = "sphere_job"
 path = os.path.join("../raw-data", folder)
 model = PerfusionGasExchangeModel(folder_path=path, params=params)
+print("Model initialised")
 model.import_mesh(
     os.path.join("../raw-data", "sphere_small.xml"), meshtype='sphere',
     type="xml"
 )
+print("Mesh imported")
 model.mesh = dolfin.refine(model.mesh)
-model.mesh = dolfin.refine(model.mesh)
+print("Mesh refined")
+print("Starting (P) simulation")
 model.sim_p(save=True, meshtype="sphere")
+print("(P) simulation done")
+print("Starting transport simulation")
 x = model.sim_sbst(hb=False, save=False)
 solution = model.sim_sbst(hb=True, save=True, guess=x)
+print("Done")
--- tests/slab_main.py
+++ tests/slab_main.py
@@ -13,14 +13,22 @@ from src.model import PerfusionGasExchangeModel
 from src.params import params
 
 
+print("Starting...")
 folder = "slab_job"
 path = os.path.join("../raw-data", folder)
 model = PerfusionGasExchangeModel(folder_path=path, params=params)
+print("Model initialised")
 model.generate_slab_mesh(
     dims=(200, 6, 6), elems=(200, 6, 6), save=True, periodic=True, refined=True
 )
+print("Mesh generated")
 model.mesh = dolfin.refine(model.mesh)
 model.mesh = dolfin.refine(model.mesh)
+print("Mesh refined")
+print("Setting u")
 model.set_u(value=(800/3, 0, 0), save=True)
+print("u set")
+print("Starting transport simulation")
 x = model.sim_sbst(hb=False, save=False)
 solution = model.sim_sbst(hb=True, save=True, guess=x)
+print("Done")
